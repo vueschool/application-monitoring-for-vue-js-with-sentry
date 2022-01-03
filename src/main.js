d2ca2797e68787cc1c6faeee8042b265d7d6862f
Daniel Kelly <me@danielkelly.io>
2022-01-03 12:28:49 -0600

    2. create a sentry project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "@/assets/main.pcss"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/main.pcss";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
 
-createApp(App)
-.use(router)
-.mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://7e17d75ec8774930a1ca95b9087ef82e@o1065263.ingest.sentry.io/6132933",
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(router).mount("#app");
