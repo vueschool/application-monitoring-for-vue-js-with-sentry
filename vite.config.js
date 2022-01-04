25e0053e6127238d90a03d4db52fe0cc727dccfb
Daniel Kelly <me@danielkelly.io>
2022-01-04 11:06:12 -0600

    4. releases in Sentry and enabling source maps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import viteSentry from "vite-plugin-sentry";
+
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, ".env.local") });
+
+const viteSentryConfig = {
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: "vue-school-course",
+  project: "sentry-course",
+  release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+  setCommits: {},
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +25,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), viteSentry(viteSentryConfig)],
+  build: {
+    sourcemap: "hidden",
+  },
+  define: {
+    __SENTRY_RELEASE__: `"${process.env.npm_package_name}@${process.env.npm_package_version}"`,
+  },
 });
