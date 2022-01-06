c1d8dc4c2823db9ac125f314a6c480b372064d28
Daniel Kelly <me@danielkelly.io>
2022-01-06 14:11:59 -0600

    8. alerts in sentry

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,13 @@
 <script setup>
 const user = {};
+const makeToast = () => {
+  sliceBread();
+};
 </script>
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
     <button @click="user.email.send()">Send Email</button>
+    <button @click="makeToast">Make Toast</button>
   </div>
 </template>
